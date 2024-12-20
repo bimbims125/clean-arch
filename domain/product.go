4307add7395da73ef8e5f5e4cb9d78b4b04d5910
bimbims125 <pramvendread@gmail.com>
2024-12-20 15:30:58 +0700

    Feat(Product): Create service get by id

--- domain/product.go
+++ domain/product.go
@@ -1,12 +1,12 @@
 package domain
 
 type Product struct {
-	ID          int
-	Name        string
-	Description string `json:"description,omitempty"`
-	Price       float64
-	ImageURL    string
-	Stock       int
-	Sold        int
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description,omitempty"`
+	Price       float64 `json:"price"`
+	ImageURL    string  `json:"image_url"`
+	Stock       int     `json:"stock"`
+	Sold        int     `json:"sold"`
 	Category    Category
 }
--- internal/repository/postgresql/product.go
+++ internal/repository/postgresql/product.go
@@ -3,6 +3,7 @@ package postgresql
 import (
 	"context"
 	"database/sql"
+	"log"
 
 	"github.com/bimbims125/clean-arch/domain"
 	"github.com/sirupsen/logrus"
@@ -34,6 +35,7 @@ func (p *ProductRepository) fetch(ctx context.Context, query string, args ...int
 		p := domain.Product{}
 		err := rows.Scan(&p.ID, &p.Name, &p.Price, &p.ImageURL, &p.Stock, &p.Sold, &p.Category.ID, &p.Category.Name)
 		if err != nil {
+			log.Println("Error while scanning product: ", err)
 			logrus.Error(err)
 			return nil, err
 		}
@@ -53,4 +55,64 @@ func (p *ProductRepository) Fetch(ctx context.Context) (result []domain.Product,
 		return nil, err
 	}
 	return res, nil
+}
+
+func (p *ProductRepository) FetchPaginated(ctx context.Context, offset, limit int) (total int, products []domain.Product, err error) {
+	err = p.Conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM products").Scan(&total)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	rows, err := p.Conn.QueryContext(ctx,
+		`SELECT
+			p.id,
+			p.name,
+			p.price,
+			p.category_id,
+			p.stock,
+			p.sold,
+			p.image_url,
+			c.name as category_name
+			FROM
+					products p
+			JOIN
+					categories c
+			ON
+					p.category_id = c.id
+			ORDER BY
+					p.id ASC
+			LIMIT
+					$1
+			OFFSET
+					$2;`, limit, offset)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var product domain.Product
+		if err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Category.ID, &product.Stock, &product.Sold, &product.ImageURL, &product.Category.Name); err != nil {
+			return 0, nil, err
+		}
+		products = append(products, product)
+	}
+
+	return total, products, nil
+}
+
+func (p *ProductRepository) GetByID(ctx context.Context, id int) (result domain.Product, err error) {
+	query := `SELECT p.id, p.name, p.price, p.image_url, p.stock, p.sold, p.category_id, c.name as category_name
+						FROM products p
+						JOIN categories c ON p.category_id = c.id
+						WHERE p.id = $1`
+	res, err := p.fetch(ctx, query, id)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	if len(res) == 0 {
+		return domain.Product{}, domain.ErrNotFound
+	}
+
+	return res[0], nil
 }
