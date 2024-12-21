870423d27677e2634325df28227e43975cde4a36
bimbims125 <pramvendread@gmail.com>
2024-12-21 17:54:56 +0700

    Feat(Category): Add category repository and handler for fetching categories

--- domain/error.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-import "errors"
-
-var ErrNotFound = errors.New("not found")
-var ErrInternalServer = errors.New("internal server error")
-var ErrBadRequest = errors.New("bad request")
