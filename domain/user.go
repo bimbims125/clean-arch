f1dbbc5ed994e22f0091eb277afddf3c65d3ded1
bimbims125 <pramvendread@gmail.com>
2024-12-25 14:38:06 +0700

    Chore(Validator): Create validations for

--- domain/user.go
+++ domain/user.go
@@ -1,9 +1,20 @@
 package domain
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password,omitempty"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password,omitempty" validate:"required,min=8,password"`
 	Role     string `json:"role"`
+}
+
+func (u *User) HashPassword() error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashed)
+	return nil
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // ResponseData represent the response data struct
 type ResponseData struct {
 	Data interface{} `json:"data"`
@@ -13,4 +18,18 @@ type ResponseError struct {
 // ResponseSuccess represent the response success struct
 type ResponseSuccess struct {
 	Message string `json:"message"`
+}
+
+func RespondWithError(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code, ResponseError{Message: message})
+}
+
+func RespondWithSuccess(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code, ResponseSuccess{Message: message})
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(payload)
 }
